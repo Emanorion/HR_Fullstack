5a671e88fff47453bac49b496514e4095062d359
Josh120604 <joshuabihute@gmail.com>
2025-07-24 11:21:03 +0200

    messaage

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,40 @@
-console.log('HI');
+import express from 'express'
+import {config} from 'dotenv'
+import mysql from 'mysql2/promise'
+
+config()
+
+
+const pool = mysql.createPool({
+
+    database:process.env.DATABASE,
+    host:process.env.HOST,
+    password:process.env.PASSWORD,
+    user:process.env.USER
+})
+
+
+const getEmployees = async ()=>{
+    // try {
+
+      let row =   await pool.query('SELECT * FROM users ')
+        return row
+    // } catch (error) {
+    //     return 'err'
+    // }
+}
+
+console.log(await getEmployees());
+
+
+
+
+const app = express()
+
+const PORT = process.env.PORT
+
+
+app.listen(PORT,()=>{
+    console.log(`http://localhost:${PORT}`);
+    
+} )
